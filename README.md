71ece18742d6c54b062c223d6ad055833ce620b1
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-02-28 07:12:37 +0000

    Update

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Lists of mini projects:
 - proj1.py : CREATING A SHOPPING CART USING PYTHON  [complete]
 - proj2.py : CREATING AN INVENTORY MANAGEMENT SYSTEM USING PYTHON  [not yet complete]
 - proj3.py : ROCK PAPER SCISSOR CUT GAME USING PYTHON  [not yet complete]
+- proj4.py : PASSWORD GENERATOR USING PYTHON [complete]
---
+++ proj4.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+password_length = int(input("Enter the length of the password: "))
+generated_password = generate_password(password_length)
+print("Generated Password:", generated_password)
+
+
+'''
+Enter the length of the password: 12
+Generated Password: M06LsxGBPsEe
+'''
